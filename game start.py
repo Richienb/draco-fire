e5f96ad64c5c9a244eb0322e096ea53afdc4d65e
Richie Bendall <richiebendall@gmail.com>
2018-05-02 17:05:24 +1200

    Update game start.py

--- game start.py
+++ game start.py
@@ -1,6 +1,16 @@
 import turtle
 import ros
+
+print("A Game By...")
+ros.delay(3)
+ros.paraspace(2)
+
 print("Shards Inc")
 ros.delay(3)
+
+print("And...")
+ros.delay(2)
+
 print("ROS Inc")
 ros.delay(3)
+
