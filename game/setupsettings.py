797a6c6f7b70f2d6d4fdc76c8e820b7cb1781b40
Richie Bendall <richiebendall@gmail.com>
2018-05-25 11:39:48 +1200

    Create setupsettings.py

---
+++ game/setupsettings.py
@@ -0,0 +1,6 @@
+import readsettings
+savename = "global.settings"
+readsettings.newfile(savename)
+readsettings.newvalue(savename, "quality", "480")
+readsettings.newvalue(savename, "sound", "50")
+readsettings.newvalue(savename, "speed", "50")
