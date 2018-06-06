ff30976dec0ebfe625117d597d5aa7173c374271
ShinyArceus1 <39779323+ShinyArceus1@users.noreply.github.com>
2018-06-07 08:41:38 +1200

    Update graphics.py

--- game/graphics.py
+++ game/graphics.py
@@ -1,13 +1,1 @@
-from turtle import *
-try:
-    color('red', 'yellow')
-    begin_fill()
-    while True:
-      forward(200)
-      left(170)
-      if abs(pos()) < 1:
-          break
-    end_fill()
-    done()
-except:
-    print("No monitor Found!")
+
