75e26caaa505be120d321e3291691cef609695f4
Richie Bendall <richiebendall@gmail.com>
2018-06-06 12:18:04 +1200

    Update part1.py

--- game/part1.py
+++ game/part1.py
@@ -1,8 +1,19 @@
 import ros
+from turtle import *
 
 print("A Game By...")
 ros.delay(3)
 ros.paraspace(2)
+
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
 
 print("shinyA Inc")
 ros.delay(3)
